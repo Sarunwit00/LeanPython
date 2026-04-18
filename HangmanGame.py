d86496ea5ac76ffe2dbcb5e4a42dd7fab016b2f5
Sarunwit00 <a.sarunwit@gmail.com>
2026-04-18 12:59:46 +0700

    Hangman 1

---
+++ HangmanGame.py
@@ -0,0 +1,14 @@
+import random
+word_list = ["apple", "baboon","camel"]
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+x = []
+for i in range(len(chosen_word)):
+    x.append("_")
+print(x)
+
+
+        
+
