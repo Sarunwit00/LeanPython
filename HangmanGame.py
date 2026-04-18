3b91cb2fcf621d5744ae613589d30f5eec318b67
Sarunwit00 <a.sarunwit@gmail.com>
2026-04-18 14:33:47 +0700

    fix HangmanGame.py successful

--- HangmanGame.py
+++ HangmanGame.py
@@ -1,14 +1,35 @@
 import random
+
+def create_Word(List):
+    word = ""
+    for i in range(len(List)):
+        word = word+List[i]
+    return word
+    
 word_list = ["apple", "baboon","camel"]
-
 chosen_word = random.choice(word_list)
-print(chosen_word)
-
-x = []
+x , y = [] , []
 for i in range(len(chosen_word)):
-    x.append("_")
-print(x)
+    y.append(chosen_word[i])
+    x.append("_")    
+# print(x)
+# print(y)
 
-
+while True:
+    letter = input("Guess a Letter : ").lower()
+    if letter == "/":
+        break
+    elif letter in y :
+        for i in range(len(y)):
+            if letter == y[i]:
+                x[i] = letter
+        print("True",end="  ")    
+        print(x)
+        if create_Word(x) == create_Word(y):
+            print("You Win")
+            break
+    else:
+        print("False",end="  ")
+        print(x)
         
 
